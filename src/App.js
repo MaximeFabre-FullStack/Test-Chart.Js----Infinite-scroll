49e1617fc76f20362300eb4bf1670bb0c76c7c98
Maxime FABRE <maxusdm06@gmail.com>
2020-05-10 19:01:34 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Bartest from "./components/BarTest";
+import PieChart from "./components/PieChart";
+import LineGraph from "./components/LineGraph";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const style = {
+  height: 30,
+  border: "1px solid green",
+  margin: 6,
+  padding: 8,
+};
+
+class App extends React.Component {
+  state = {
+    items: Array.from({ length: 20 }),
+  };
+
+  fetchMoreData = () => {
+    // a fake async api call like which sends
+    // 20 more records in 1.5 secs
+    setTimeout(() => {
+      this.setState({
+        items: this.state.items.concat(Array.from({ length: 20 })),
+      });
+    }, 1500);
+  };
+  render() {
+    return (
+      <div className="App">
+        <Bartest></Bartest>
+        <PieChart></PieChart>
+        <LineGraph></LineGraph>
+        <InfiniteScroll
+          dataLength={this.state.items.length}
+          next={this.fetchMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {this.state.items.map((i, index) => (
+            <div style={style} key={index}>
+              div - #{index}
+            </div>
+          ))}
+        </InfiniteScroll>
+      </div>
+    );
+  }
 }
 
 export default App;
